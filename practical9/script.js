49066387cd4356cc038cc2ecc721f2b8ed0f427c
shwetapawar-712 <sdpawar712@gmail.com>
2024-12-16 19:43:13 +0530

    Update script.js

--- practical9/script.js
+++ practical9/script.js
@@ -1,18 +1,18 @@
 
-function checkEvenOdd(number) {
-    if (number % 2 === 0) {
-      console.log (number+" is even.");
-    } else {
-      console.log(number + " is odd.");
-    }
-  }
-  
-  let inputNumber = prompt("Enter a number:");
-  inputNumber = parseInt(inputNumber);
-  
-  if (Number.isInteger(inputNumber)) {
-    checkEvenOdd(inputNumber);
-  } else {
-    console.log("Please enter a valid number.");
-  }
-  
+ function checkEvenOdd() {
+            // Get the number entered by the user
+            var num = document.getElementById("number").value;
+
+            // Check if the number is valid
+            if (num === "") {
+                document.getElementById("result").textContent = "Please enter a number.";
+                return;
+            }
+
+            // Check if the number is even or odd
+            if (num % 2 === 0) {
+                document.getElementById("result").textContent = num + " is even.";
+            } else {
+                document.getElementById("result").textContent = num + " is odd.";
+            }
+        }
